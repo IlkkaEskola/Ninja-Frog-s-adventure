ed1cc24dfb6a430cd9c7076ac766c69d63fbfa08
IlkkaEskola <ilkka.eskola@yahoo.com>
2023-06-26 22:59:44 +0300

    Scorpions added

---
+++ Assets/Scripts/ScorpionMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorpionMovement : MonoBehaviour
+{
+    public Transform startPoint; // alku piste
+    public Transform endPoint;   // loppupiste
+    public float speed = 2.0f;   // nopeus liikkuessa
+    public float stoppingDistance = 0.1f; // pysäytys etäisyys
+
+    private Vector3 currentTarget;
+    private int direction = 1;
+
+    //private Animator animator;
+
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+        currentTarget = endPoint.position;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, currentTarget) < stoppingDistance)
+        {
+            if (currentTarget == endPoint.position)
+            {
+                currentTarget = startPoint.position;
+                direction = -1;
+            }
+            else
+            {
+                currentTarget = endPoint.position;
+                direction = 1;
+            }
+
+            // käännä ympäri
+            Vector3 newScale = transform.localScale;
+            newScale.x = direction;
+            transform.localScale = newScale;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+        //animator.SetBool("Scorpion", true);
+    }
+}
