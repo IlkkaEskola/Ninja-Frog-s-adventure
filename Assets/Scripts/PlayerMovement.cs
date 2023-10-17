993da5f747e099690bc223eaa09cc6c664ec3f39
IlkkaEskola <ilkka.eskola@yahoo.com>
2023-10-17 14:25:58 +0300

    Level4 update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -91,6 +91,7 @@ public class PlayerMovement : MonoBehaviour
         //}
     }
 
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Trap"))
