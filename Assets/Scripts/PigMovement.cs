eba5955b96bcb654e69ed224a0850d5b632d7750
IlkkaEskola <ilkka.eskola@yahoo.com>
2023-10-25 14:55:53 +0300

    Added Pig enemy and script

--- Assets/Scripts/PigMovement.cs
+++ Assets/Scripts/PigMovement.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class PigMovement : MonoBehaviour
 {
+    private Rigidbody2D rb2D;
+
     public Transform startPoint; // alku piste
     public Transform endPoint;   // loppupiste
     public float speed = 2.0f;   // nopeus liikkuessa
     public float stoppingDistance = 0.1f; // pysäytys etäisyys
+    
+    public float jumpForce = 6f;
+    public float jumpInterval = 3f;
+    private bool isJumping = false;
+    private float jumpTimer = 0f;
 
     private Vector3 currentTarget;
     private int direction = 1;
@@ -17,6 +24,7 @@ public class EnemyMovement : MonoBehaviour
     void Start()
     {
         //animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
         currentTarget = endPoint.position;
     }
 
@@ -42,6 +50,32 @@ public class EnemyMovement : MonoBehaviour
         }
 
         transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
-        
+
+        jumpTimer += Time.deltaTime;
+
+        if (jumpTimer >= jumpInterval)
+        {
+            Jump();
+            jumpTimer = 0f;
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (!isJumping)
+        {
+            rb2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+            StartCoroutine(ResetJump());
+        }
+    }
+
+    IEnumerator ResetJump()
+    {
+        yield return new WaitForSeconds(0.5f); // Adjust as needed
+        isJumping = false;
     }
 }
+
+
