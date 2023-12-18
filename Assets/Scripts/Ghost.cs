50e5d6e117659c54bf09d16c62e77612df35ea1e
IlkkaEskola <ilkka.eskola@yahoo.com>
2023-12-18 23:24:04 +0200

    Level2 update
    Ghost enemy done

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -2,34 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arrow : MonoBehaviour
+public class Ghost : MonoBehaviour
 {
-    public GameObject arrowPrefab;
+    public GameObject ghostPrefab;
     public float spawnInterval = 1.5f;
     public float speed;
     private float yMin = -3.5f;
     private float yMax = -1.5f;
 
+    
+
     void Start()
-    {
-        InvokeRepeating("SpawnArrow", 2f, spawnInterval);
-        
+    {    
+        InvokeRepeating("SpawnGhost", 2f, spawnInterval);
+        Destroy(gameObject, 2.2f);
     }
 
 
     void Update()
     {
         transform.Translate(-speed * Time.deltaTime, 0, 0);
-        Destroy(gameObject, 2f);
     }
 
-    void SpawnArrow()
+    void SpawnGhost()
     {
-        // Instantiate a snowball prefab at a random position
-        float posX = 82f;
+        float posX = 83f;
         float randomPosY = Random.Range(yMax, yMin);
         Vector2 spawnPosition = new Vector2(posX, randomPosY);
 
-        Instantiate(arrowPrefab, spawnPosition, Quaternion.identity);
+        Instantiate(ghostPrefab, spawnPosition, Quaternion.identity);
     }
 }
